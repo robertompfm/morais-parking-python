5cdbb6913395c8ae5f7eea76e48192cdf93f4603
larissacauane <61204154+larissacauane@users.noreply.github.com>
2020-05-22 19:44:18 -0300

    Update main.py

--- MoraisParkingPython/view/main.py
+++ MoraisParkingPython/view/main.py
@@ -1,7 +1,8 @@
-from model.veiculo import Veiculo
-from model.proprietario import Proprietario
-from model.area_estacionamento import AreaEstacionamento
-from model.estacionamento import Estacionamento
+from model.Veiculo import Veiculo
+from model.Proprietario import Proprietario
+from model.AreaEstacionamento import AreaEstacionamento
+from model.Estacionamento import Estacionamento
+from model.Usuario import Usuario
 
 morais_parking = Estacionamento(30)
 area_estacionamento = AreaEstacionamento("Carros", 10)
@@ -9,15 +10,62 @@ carro = Veiculo("Larissa", 20192007035, "Sistemas para Internet", "Comum", "ABC1
 
 
 menu = "Escolha uma opção: "
-def opcoes():
-    while menu != 5:
+
+def menu_RH():
+    while menu != 3:
+        print("====== MORAIS PARKING ======")
+        print("[1] Veículo",
+              "\n[2] Proprietário",
+              "\n[3] Sair")
+
+        op_menu = int(input(menu))
+        if op_menu == 1:
+            mp_veiculo()
+        elif op_menu == 2:
+            mp_proprietario()
+        else:
+            break
+
+def menu_Estacionamento():
+    while menu != 6:
+        print("====== MORAIS PARKING ======")
+        print("[1]Estacionamento",
+              "\n[2] Veículo",
+              "\n[3] Proprietário",
+              "\n[4] Ocorrências"
+              "\n[5] Eventos"
+              "\n[6] Sair")
+
+        op_menu = int(input(menu))
+        if op_menu == 1:
+            mp_estacionamento()
+
+        elif op_menu == 2:
+            mp_veiculo()
+
+        elif op_menu == 3:
+            mp_proprietario()
+
+        elif op_menu == 4:
+            mp_ocorrencias()
+
+        elif op_menu == 5:
+            mp_eventos()
+
+        else:
+            break
+
+def menu_Gestor():
+    while menu != 7:
         print("====== MORAIS PARKING ======")
         print(
-            "\n[1] Estacionamento",
+            "[1] Estacionamento",
             "\n[2] Veículo",
             "\n[3] Proprietário",
             "\n[4] Áreas",
-            "\n[5] Sair"
+            "\n[5] Ocorrências"
+            "\n[6] Eventos"
+            "\n[7] Sair"
         )
 
         op_menu = int(input(menu))
@@ -32,13 +80,27 @@ def opcoes():
 
         elif op_menu == 4:
             mp_areas()
+
+        elif op_menu == 5:
+            mp_ocorrencias()
+
+        elif op_menu == 6:
+            mp_eventos()
+
         else:
             break
+
+def login():
+    print("====== MORAIS PARKING ======")
+    print("[1] Entrar", "\n[2] Cadastar Usuário")
+
+    op_menu = int(input(menu))
+
 
 
 def mp_estacionamento():
     print(
-        "\n[1] Autorizar Entrada",
+        "  [1] Autorizar Entrada",
         "\n[2] Autorizar Saída",
         "\n[3] Busca Veículo",
         "\n[4] Busca Proprietário",
@@ -176,7 +238,57 @@ def mp_areas():
     else:
         print("Opção Inválida")
 
+def mp_ocorrencias():
+    print("[1] Cadastrar Ocorrência", "\n[2] Buscar Ocorrência")
+    op_menu = int(input(menu))
+
+    if op_menu == 1:
+        tipo = input("Tipo da ocorrência: ")
+        placa = input("Placa: ")
+        quantVeiculo = input("Quantidade de veículo: ")
+        data = input("Data: ")
+        hora = input("Hora: ")
+        descricao = input("Descrição: ")
+        if morais_parking.cadastro_ocorrencia(tipo, placa, quantVeiculo, data, hora, descricao):
+            print("Ocorrência cadastrada")
+        else:
+            print("Não foi possível cadastrar a ocorrência")
+
+    elif op_menu == 2:
+        tipo = input("Tipo da ocorrência: ")
+        ocorrencia = morais_parking.identificar_ocorrencia(tipo)
+        if ocorrencia == None:
+            print("Ocorrência não encontrada")
+        else:
+            print(ocorrencia)
+
+def mp_eventos():
+    print("[1] Cadastrar Evento", "\n[2] Buscar Evento")
+    op_menu = input(menu)
+
+    if op_menu == 1:
+        nome_evento = input("Nome do evento: ")
+        data_inicial = input("Data inicial: ")
+        data_final = input("Data final: ")
+        vagas = input("Quantidade de vagas: ")
+        if morais_parking.cadastro_evento(nome_evento, data_inicial, data_final, vagas):
+            print("Evento cadastrado")
+        else:
+            print("Não foi possível cadastrar o evento")
+    elif op_menu == 2:
+        nome_evento = input("Nome do evento: ")
+        evento = morais_parking.identificar_evento(nome_evento)
+        if evento == None:
+            print("Evento não encontrado")
+        else:
+            print(evento)
+
+
+
+
+
 
 def main():
-    opcoes()
+    login()
 main()
+
