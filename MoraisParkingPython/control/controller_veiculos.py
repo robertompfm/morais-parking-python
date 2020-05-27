e1582190a98fd5e1d7681c1be4fe826d760808da
larissacauane <61204154+larissacauane@users.noreply.github.com>
2020-05-27 18:01:12 -0300

    Add files via upload

---
+++ MoraisParkingPython/control/controller_veiculos.py
@@ -0,0 +1,38 @@
+from dao.data_veiculos import DataVeiculos
+from model.Veiculo import Veiculo
+
+class ControllerVeiculos():
+
+    def __init__(self):
+        self.data_veiculos = DataVeiculos()
+
+
+    def register_veiculo(self, placa, modelo, cor, proprietario, nome_area):
+        self.data_veiculos.open()
+        veiculo = Veiculo(placa, modelo, cor, proprietario, nome_area)
+        success = self.data_veiculos.insert_veiculo(veiculo)
+        if success:
+            print("Veículo cadastrado com sucesso!")
+        else:
+            print("Não foi possível cadastrar o veículo")
+        self.data_veiculos.close()
+        return success
+
+    def remove_veiculo(self, placa):
+        self.data_veiculos.open()
+        veiculo = self.data_veiculos.delete_veiculo_by_placa(placa)
+        if veiculo:
+            print("Veículo foi removido")
+        else:
+            print("Não foi possivel remover veículo")
+        self.data_veiculos.close()
+        return veiculo
+
+    def find_veiculo(self, placa):
+        self.data_veiculos.open()
+        veiculo = self.data_veiculos.query_veiculo_by_placa(placa)
+        if veiculo is None:
+            print("Veículo não cadastrado")
+        self.data_veiculos.close()
+        return veiculo
+
