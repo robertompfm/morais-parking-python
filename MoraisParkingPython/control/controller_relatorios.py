915456ecdae438f8a86b6d26fb1055f32e3cef69
larissacauane <61204154+larissacauane@users.noreply.github.com>
2020-06-02 21:39:14 -0300

    add relatorios

---
+++ MoraisParkingPython/control/controller_relatorios.py
@@ -0,0 +1,29 @@
+from model.Estacionamento import Estacionamento
+estacionamento = Estacionamento(30)
+
+def status_estacionamento():
+    arquivo = open('status_estacionamento.txt', 'w')
+    #arquivo.write('Morais Parking\n')
+    arquivo.write(estacionamento.get_status())
+    arquivo.close()
+
+    arquivo = open('status_estacionamento.txt', 'r')
+    for linha in arquivo:
+        linha = linha.rstrip()
+        print(linha)
+    arquivo.close()
+
+
+def main():
+    print(status_estacionamento())
+
+main()
+
+
+
+
+
+
+
+
+
